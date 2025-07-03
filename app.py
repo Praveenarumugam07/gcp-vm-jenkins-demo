f071afc9042a2b2b3c75348ec1aff3236462c4c3
Praveenarumugam07 <96639707+Praveenarumugam07@users.noreply.github.com>
2025-07-03 14:53:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from GCP VM deployed via Jenkins!"
+    return "Hello from GCP VM deployed via Jenkins"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
